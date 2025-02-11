d89653c546915a80ce04cc58a441a6aa9e731a15
Rafael Queiróz <60499241+rafaelq80@users.noreply.github.com>
2025-02-11 15:34:21 -0300

    ✨ CSS Responsivo para Tablet e comentários no Script JavaScript

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,23 @@
+// Seleciona a Seção about
 const sobre = document.querySelector("#about");
+
+// Seleciona o Formulário
 const formulario = document.querySelector("#formulario");
+
+// Expressão Regular para validar o e-mail
 const emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 
+// Função para consumir os dados do Github
 async function getApiGithub(){
     try{
 
+        // Envia a Requisição HTTP
         const dadosPerfil = await fetch(`https://api.github.com/users/rafaelq80`);
+        
+        // Converte a resposta para o formato JSON
         const perfil = await dadosPerfil.json();
 
+        // Cria o conteúdo da Seção About com os dados da Reposta da Requisição
         let conteudo = `
         
              <!-- Imagem da Seção Sobre -->
@@ -33,6 +43,7 @@ async function getApiGithub(){
         
         `;
 
+        // Adiciona o conteúdo na página HTML, na Seção About
         sobre.innerHTML += conteudo;
 
     }catch(error){
@@ -40,24 +51,40 @@ async function getApiGithub(){
     }
 }
 
+// Validação do Formulário antes do envio
 formulario.addEventListener("submit", function(event){
+    
+    // Impede que o formulário seja enviado antes da validação
     event.preventDefault();
 
+    // Seleciona os elementos do campo nome (input e span)
     const campoNome = document.querySelector("#nome");
     const txtNome = document.querySelector("#txtNome");
 
+    // Verifica se o campo possui menos de 3 caracteres
     if(campoNome.value.length < 3){
+
+        // Caso afirmativo, exibe a mensagem de erro no span
         txtNome.innerHTML = "O Nome deve ter no mínimo 3 caracteres.";
+
+        // Mantém o cursor no campo
         campoNome.focus();
+
+        // Finaliza a função
         return;
     }else{
+        // Senão, limpa o span
         txtNome.innerHTML = "" ;
     }
 
+    // Seleciona os elementos do campo email (input e span)
     const campoEmail = document.querySelector("#email");
     const txtEmail = document.querySelector("#txtEmail");
 
+    // Valida através da Expressão Regular se o usuário digitou um e-mail válido
     if(!campoEmail.value.match(emailRegex)){
+
+         // Caso afirmativo, exibe a mensagem de erro no span
         txtEmail.innerHTML = "Digite um e-mail válido.";
         campoEmail.focus();
         return;
@@ -65,10 +92,14 @@ formulario.addEventListener("submit", function(event){
         txtEmail.innerHTML = "" ;
     }
 
+    // Seleciona os elementos do campo assunto (input e span)
     const campoAssunto = document.querySelector("#assunto");
     const txtAssunto = document.querySelector("#txtAssunto");
 
+    // Verifica se o campo possui pelo menos 5 caracteres
     if(campoAssunto.value.length < 5){
+
+         // Caso afirmativo, exibe a mensagem de erro no span
         txtAssunto.innerHTML = "O assunto deve ter no mínimo 5 caracteres.";
         campoAssunto.focus();
         return;
@@ -76,8 +107,10 @@ formulario.addEventListener("submit", function(event){
         txtAssunto.innerHTML = "" ;
     }
 
+    // Envia o formulário caso os 3 campos sejam validados
     formulario.submit();
     
 });
 
+// Executa a função ao carregar o script, gerando a Seção About
 getApiGithub();
